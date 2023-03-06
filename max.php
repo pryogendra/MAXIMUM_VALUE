778a8980cf22327658847ff0ed1d234305cb40cd
Yogendra Prasad <127005415+pryogendra@users.noreply.github.com>
2023-03-06 09:41:40 +0530

    Add files via upload

---
+++ max.php
@@ -0,0 +1,7 @@
+<html>
+    <?php
+    print(max(123,234,65,457,122,23))."<br>";  //max() returns the maximum value 
+    print(max(1,34,5,7,12,2))."<br>";
+    print(max(13,24,6,57,12,23))."<br>";
+    ?>
+</html>
